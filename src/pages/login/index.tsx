135ee51dc92451ba8a49652355613b2df6715132
caiuby7 <caiuby7@gmail.com>
2025-05-28 11:29:42 -0300

    feat: redesign login page - Add background image and logo - Make form background transparent - Add icons to inputs - Change color scheme to #9d2235 - Increase form size by 20% - Update title to 'Pesquisa NPS'

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,8 +3,9 @@ import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/router'
 import {
   Box, Button, Input, Heading, Text,
-  Stack
+  Stack, Image, Flex
 } from '@chakra-ui/react'
+import { MdEmail, MdLock } from 'react-icons/md'
 
 type LoginData = {
   email: string
@@ -28,21 +29,67 @@ export default function LoginPage() {
   }
 
   return (
-    <Box maxW="md" mx="auto" mt={20} p={8} borderWidth={1} borderRadius="lg">
-      <Heading mb={6}>Admin</Heading>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack mb={4}>
-          <Text>Email</Text>
-          <Input type="email" {...register('email')} required />
-        </Stack>
-        <Stack mb={6}>
-          <Text>Senha</Text>
-          <Input type="password" {...register('password')} required />
-        </Stack>
-        <Button colorScheme="purple" type="submit" width="full">
-          Entrar
-        </Button>
-      </form>
+    <Box
+      minH="100vh"
+      bgImage="url('/background.jpg')"
+      bgSize="cover"
+      backgroundPosition="center"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Image src="/logo.png" alt="Logo" mb={8} maxW="200px" />
+      <Box 
+        maxW="600px" 
+        p={10} 
+        borderWidth={1} 
+        borderRadius="lg" 
+        bg="rgba(255, 255, 255, 0.3)" 
+        backdropFilter="blur(8px)"
+        shadow="lg"
+      >
+        <Heading mb={8} color="#9d2235" fontSize="2xl">Pesquisa NPS</Heading>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack mb={6}>
+            <Flex align="center" bg="white" borderRadius="md">
+              <Box p={3}>
+                <MdEmail color="#9d2235" size={24} />
+              </Box>
+              <Input 
+                type="email" 
+                {...register('email')} 
+                required 
+                placeholder="Digite seu email"
+                border="none"
+                _focus={{ border: "none" }}
+                fontSize="lg"
+                py={6}
+              />
+            </Flex>
+          </Stack>
+          <Stack mb={8}>
+            <Flex align="center" bg="white" borderRadius="md">
+              <Box p={3}>
+                <MdLock color="#9d2235" size={24} />
+              </Box>
+              <Input 
+                type="password" 
+                {...register('password')} 
+                required 
+                placeholder="Digite sua senha"
+                border="none"
+                _focus={{ border: "none" }}
+                fontSize="lg"
+                py={6}
+              />
+            </Flex>
+          </Stack>
+          <Button bg="#9d2235" _hover={{ bg: "#7a1a29" }} color="white" type="submit" width="full" py={6} fontSize="lg">
+            Entrar
+          </Button>
+        </form>
+      </Box>
     </Box>
   )
 }
